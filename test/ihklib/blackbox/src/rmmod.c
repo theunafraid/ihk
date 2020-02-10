754275b17217b1da7e292dfbfc01b490a2891ca2
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-10 06:16:23 +0000

    Fix rmmod.c
    Change-Id: Ia475e2e1de5236bec6e7a935d9d619ef97410529

--- test/ihklib/blackbox/src/rmmod.c
+++ test/ihklib/blackbox/src/rmmod.c
@@ -7,66 +7,68 @@
 #include "okng.h"
 #include "init_fini.h"
 
-static int mod_loaded(const char *expected) {
+static int mod_loaded(const char *name)
+{
 	int count = 0; 
-	int ret = 0;
+	int ret;
 	FILE *st = NULL;
 	char cmd[1024];
-	char module[64];
 
-	sprintf(cmd, "lsmod | awk '{print $1}' | grep %s", expected);
-	
+	sprintf(cmd, "lsmod | cut -d' ' -f1 | grep -c -x %s", name);
+
 	if ((st = popen(cmd, "r")) == NULL) {
 		INFO("%s Failed\n", cmd);
 		goto out;
 	}
-	while ((fscanf(st, "%s", module)) > 0) {
-		if (strcmp(expected, module) == 0) {
-			ret = 1;
-			break;
-		}
-		count = strlen(module);
-		fseek(st, count, SEEK_CUR);
-	}	
-	
+	ret = fscanf(st, "%d\n", &count);
 	pclose(st);
-out:	
-	return ret;
+
+out:
+	return count;
 }
 
 int rmmod(int verbose)
 {
 	int ret;
 	char cmd[1024];
+	char name[1024];
 
-	sprintf(cmd, "rmmod %s/kmod/mcctrl.ko", QUOTE(WITH_MCK));
-	if (verbose)
-		INFO("%s\n", cmd);
-	ret = system(cmd);
-	ret = WEXITSTATUS(ret);
-	if (ret != 0) {
-		INFO("%s returned %d\n", cmd, ret);
+	if (mod_loaded("mcctrl")) {
+
+		sprintf(cmd, "rmmod %s/kmod/mcctrl.ko", QUOTE(WITH_MCK));
+		if (verbose)
+			INFO("%s\n", cmd);
+		ret = system(cmd);
+		ret = WEXITSTATUS(ret);
+		if (ret != 0) {
+			INFO("%s returned %d\n", cmd, ret);
+		}
 	}
 
-	sprintf(cmd, "rmmod %s/kmod/ihk-%s.ko",
-		QUOTE(WITH_MCK), QUOTE(BUILD_TARGET));
-	if (verbose)
-		INFO("%s\n", cmd);
-	ret = system(cmd);
-	ret = WEXITSTATUS(ret);
-	if (ret != 0) {
-		INFO("%s returned %d\n", cmd, ret);
+	sprintf(name, "ihk_%s", QUOTE(KMOD_POSTFIX));
+	if (mod_loaded(name)) {
+
+		sprintf(cmd, "rmmod %s/kmod/ihk-%s.ko",
+			QUOTE(WITH_MCK), QUOTE(BUILD_TARGET));
+		if (verbose)
+			INFO("%s\n", cmd);
+		ret = system(cmd);
+		ret = WEXITSTATUS(ret);
+		if (ret != 0) {
+			INFO("%s returned %d\n", cmd, ret);
+		}
 	}
 
-	sprintf(cmd, "rmmod %s/kmod/ihk.ko", QUOTE(WITH_MCK));
-	if (verbose)
-		INFO("%s\n", cmd);
-	ret = system(cmd);
-	ret = WEXITSTATUS(ret);
-	if (ret != 0) {
-		INFO("%s returned %d\n", cmd, ret);
+	if (mod_loaded("ihk")) {
+		sprintf(cmd, "rmmod %s/kmod/ihk.ko", QUOTE(WITH_MCK));
+		if (verbose)
+			INFO("%s\n", cmd);
+		ret = system(cmd);
+		ret = WEXITSTATUS(ret);
+		if (ret != 0) {
+			INFO("%s returned %d\n", cmd, ret);
+		}
 	}
-
 out:
 	return ret;
 }
