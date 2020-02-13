77ababda263fb00ac3c547d5fd711faeb22ab2c7
Jie Yin <jie.yin@riken.jp>
2020-02-13 07:22:19 +0000

    Add ihk_os_release_mem01
    Change-Id: I05451435756fa006800649c0fcc8fe9f2f3f62af

--- test/ihklib/blackbox/src/ihk_os_release_mem01.c
+++ test/ihklib/blackbox/src/ihk_os_release_mem01.c
@@ -6,9 +6,10 @@
 #include "params.h"
 #include "init_fini.h"
 
-const char *messages[] = {
-	"before insmod",
-	"after insmod",
+const char param[] = "existence of os instance";
+const char *values[] = {
+	"before ihk_create_os()",
+	"after ihk_create_os()",
 };
 
 int main(int argc, char **argv)
@@ -18,69 +19,69 @@ int main(int argc, char **argv)
 
 	params_getopt(argc, argv);
 
-	/* All of McKernel CPUs */
-	struct mems mems_input_reserve_cpu[2] = { 0 };
-	for (i = 1; i < 2; i++) {
-		ret = mem_chunks_ls(&mems_input_reserve_cpu[i]);
-		INTERR(ret, "mem_chunks_ls returned %d\n", ret);
+	ret = insmod(params.uid, params.gid);
+	INTERR(ret, "insmod returned %d\n", ret);
 
-		ret = mems_shift(&mems_input_reserve_cpu[i], 2);
-		INTERR(ret, "mems_shift returned %d\n", ret);
-	}
+	ret = mems_reserve();
+	INTERR(ret, "mems_reserve returned %d\n", ret);
 
 	struct mems mems_input[2] = { 0 };
-	for (i = 1; i < 2; i++) {
-		ret = mem_chunks_ls(&mems_input[i]);
-		INTERR(ret, "mem_chunks_ls returned %d\n", ret);
+	struct mems mems_input_assign_mem[2] = { 0 };
+	struct mems mems_after_release[] = { 0 };
 
-		ret = mems_shift(&mems_input[i], 2);
-		INTERR(ret, "mems_shift returned %d\n", ret);
+	for (i = 1; i < 2; i++) {
+		ret = mems_reserved(&mems_input[i]);
+		INTERR(ret, "mems_reserved returned %d\n", ret);
+
+		ret = mems_reserved(&mems_input_assign_mem[i]);
+		INTERR(ret, "mems_reserved returned %d\n", ret);
+
+		ret = mems_reserved(&mems_after_release[i]);
+		INTERR(ret, "mems_reserved returned %d\n", ret);
 	}
 
-	int ret_expected_reserve_cpu[] = { -ENOENT, 0 };
-	int ret_expected[] = { -ENOENT, 0 };
+	ret = mems_shift(&mems_after_release[1],
+			mems_after_release[1].num_mem_chunks);
+	INTERR(ret, "mems_shift returned %d\n", ret);
 
-	struct mems mems_after_release[] = {
-		 { 0 },
-		 { .mem_chunks = NULL, .num_mem_chunks = 0 },
-		};
+	int ret_expected_assign_mem[2] = { -ENOENT, 0 };
+	int ret_expected[2] = { -ENOENT, 0 };
+
 
 	struct mems *mems_expected[] = {
-		 NULL,
-		 &mems_after_release[1],
-		};
+		NULL,
+		&mems_after_release[1],
+	};
 
 	/* Activate and check */
 	for (i = 0; i < 2; i++) {
-		START("test-case: /dev/mcd0: %s\n", messages[i]);
+		START("test-case: %s: %s\n", param, values[i]);
 
-		ret = ihk_reserve_cpu(0, mems_input_reserve_cpu[i].mem_chunks,
-				      mems_input_reserve_cpu[i].num_mem_chunks);
-		INTERR(ret != ret_expected_reserve_cpu[i],
-		       "ihk_reserve_cpu returned %d\n", ret);
+		ret = ihk_os_assign_mem(0, mems_input_assign_mem[i].mem_chunks,
+				mems_input_assign_mem[i].num_mem_chunks);
+		INTERR(ret != ret_expected_assign_mem[i],
+		       "ihk_os_assign_mem returned %d\n", ret);
 
-		ret = ihk_release_cpu(0, mems_input[i].mem_chunks,
+		ret = ihk_os_release_mem(0, mems_input[i].mem_chunks,
 				      mems_input[i].num_mem_chunks);
 		OKNG(ret == ret_expected[i],
 		     "return value: %d, expected: %d\n",
 		     ret, ret_expected[i]);
 
-		if (mem_chunks_expected[i]) {
-			ret = mem_chunks_check_reserved(mems_expected[i]);
+		if (mems_expected[i]) {
+			ret = mems_check_assigned(mems_expected[i]);
 			OKNG(ret == 0, "reserved as expected\n");
-
-			/* Clean up */
-			ret = ihk_release_cpu(0, mems_after_release[i].mem_chunks,
-					      mems_after_release[i].num_mem_chunks);
-			INTERR(ret, "ihk_release_cpu returned %d\n", ret);
 		}
 
 		/* Precondition */
 		if (i == 0) {
-			ret = insmod(params.uid, params.gid);
-			INTERR(ret == 0, "insmod returned %d\n", ret);
+			ret = ihk_create_os(0);
+			INTERR(ret, "ihk_create_os returned %d\n", ret);
 		}
 	}
+
+	ret = mems_release();
+	INTERR(ret, "mems_release returned %d\n", ret);
 
 	ret = 0;
  out:
