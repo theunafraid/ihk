0f8ac09a257c8e251850960f1edbd1ae9849e349
Jie Yin <jie.yin@riken.jp>
2020-02-13 07:42:39 +0000

    Add ihk_release_mem01
    Change-Id: Ibe40e7030b263df4c2576dc2b3603a4c64f5673a

--- test/ihklib/blackbox/src/ihk_release_mem01.c
+++ test/ihklib/blackbox/src/ihk_release_mem01.c
@@ -19,29 +19,58 @@ int main(int argc, char **argv)
 
 	params_getopt(argc, argv);
 
-	/* Precondition */
-	ret = insmod(params.uid, params.gid);
-	INTERR(ret, "insmod returned %d\n", ret);
-	
-	ret = mems_reserve();
-	INTERR(ret, "mems_reserve returned %d\n", ret);
-	
+	struct mems mems_input_reserve_mem[2] = { 0 };
+
+	for (i = 0; i < 2; i++) {
+		int excess;
+
+		ret = mems_ls(&mems_input_reserve_mem[i], "MemFree", 0.9);
+		INTERR(ret, "mems_ls returned %d\n", ret);
+
+		excess = mems_input_reserve_mem[i].num_mem_chunks - 4;
+		if (excess > 0) {
+			ret = mems_shift(&mems_input_reserve_mem[i], excess);
+			INTERR(ret, "mems_shift returned %d\n", ret);
+		}
+	}
+
 	struct mems mems_input[2] = { 0 };
-	
-	int ret_expected[] = { 0, -ENOENT };
+
+	int ret_expected[] = { -ENOENT, 0 };
+	int ret_expected_reserve_mem[] = { -ENOENT, 0 };
+
+	struct mems mems_after_release[] = {
+		{ 0 },
+		{ .mem_chunks = NULL, .num_mem_chunks = 0 },
+	};
+
+	struct mems *mems_expected[] = {
+		NULL,
+		&mems_after_release[1],
+	};
 
 	/* Activate and check */
 	for (i = 0; i < 2; i++) {
-		
-		ret = mems_reserved(&mems_input[i]);
-		INTERR(ret, "mems_reserved returned %d\n", ret);
-
-		if ( i == 1 ) {
-			ret = rmmod(0);
-			INTERR(ret, "rmmod returned %d\n", ret);
-		}
-
 		START("test-case: %s: %s\n", param, values[i]);
+
+		ret = ihk_reserve_mem(0, mems_input_reserve_mem[i].mem_chunks,
+				      mems_input_reserve_mem[i].num_mem_chunks);
+		INTERR(ret != ret_expected_reserve_mem[i],
+		       "ihk_reserve_mem returned %d\n", ret);
+
+		if (ret_expected_reserve_mem[i] == 0) {
+			ret = ihk_get_num_reserved_mem_chunks(0);
+			INTERR(ret < 0,
+			       "ihk_get_num_reserved_mem_chunks returned %d\n",
+			       ret);
+
+			ret = mems_init(&mems_input[i], ret);
+			INTERR(ret, "mems_init returned %d\n", ret);
+
+			ret = ihk_query_mem(0, mems_input[i].mem_chunks,
+					    mems_input[i].num_mem_chunks);
+			INTERR(ret, "ihk_query_mem returned %d\n", ret);
+		}
 
 		ret = ihk_release_mem(0, mems_input[i].mem_chunks,
 				      mems_input[i].num_mem_chunks);
@@ -49,9 +78,15 @@ int main(int argc, char **argv)
 		     "return value: %d, expected: %d\n",
 		     ret, ret_expected[i]);
 
+		if (mems_expected[i]) {
+			ret = mems_check_reserved(mems_expected[i], NULL);
+			OKNG(ret == 0, "released as expected\n");
+		}
+
 		if (i == 0) {
-			ret = mems_reserve();
-			INTERR(ret, "mems_reserve returned %d\n", ret);
+			/* Precondition */
+			ret = insmod(params.uid, params.gid);
+			INTERR(ret, "insmod returned %d\n", ret);
 		}
 	}
 
