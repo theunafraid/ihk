5240445e28aec94b0058979de6800ace17457698
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-16 10:40:14 +0000

    Add ihk_os_shutdown08
    Change-Id: Ic34a8129db79146fae8fc7e37d9b5d1080ad16b8

---
+++ test/ihklib/blackbox/src/ihk_os_shutdown08.c
@@ -0,0 +1,266 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <ihklib.h>
+#include <ihk/ihklib_private.h>
+#include "util.h"
+#include "okng.h"
+#include "cpu.h"
+#include "mem.h"
+#include "os.h"
+#include "user.h"
+#include "params.h"
+#include "mod.h"
+
+const char param[] = "LWK status";
+const char *messages[] = {
+	"IHK_STATUS_INACTIVE"
+	" (before smp_ihk_os_boot or after smp_ihk_destroy_os)",
+	"IHK_STATUS_BOOTING"
+	" (smp_ihk_os_boot -- arch_init -- arch_ready -- done_init)",
+	"IHK_STATUS_RUNNING (after done_init)",
+	"IHK_STATUS_SHUTDOWN (smp_ihk_os_shutdown -- smp_ihk_destroy_os)",
+	"IHK_STATUS_PANIC",
+	"IHK_STATUS_HUNGUP",
+	"IHK_STATUS_FREEZING",
+	"IHK_STATUS_FROZEN"
+};
+
+int wait_for_status(int status)
+{
+	int ret;
+	int i;
+
+	INFO("waiting for status getting %d\n",
+	     status);
+
+	for (i = 0; i < 20; i++) {
+		ret = ihk_os_get_status(0);
+		if (ret == status) {
+			ret = 0;
+			goto out;
+		}
+		printf("status: %d\n", ret);
+		usleep(400000);
+	}
+	ret = -ETIMEDOUT;
+ out:
+	return ret;
+}
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int i, j;
+	pid_t pid = -1;
+
+	params_getopt(argc, argv);
+
+	/* Precondition */
+	ret = insmod(params.uid, params.gid);
+	INTERR(ret, "insmod returned %d\n", ret);
+
+	ret = cpus_reserve();
+	INTERR(ret, "cpus_reserve returned %d\n", ret);
+
+	ret = mems_reserve();
+	INTERR(ret, "mems_reserve returned %d\n", ret);
+
+	enum ihklib_os_status target_status[] = {
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_BOOTING,
+		IHK_STATUS_RUNNING,
+		IHK_STATUS_SHUTDOWN, /* shutting-down */
+		IHK_STATUS_PANIC,
+		IHK_STATUS_HUNGUP,
+		IHK_STATUS_FREEZING,
+		IHK_STATUS_FROZEN,
+	};
+
+	int ret_expected[] = {
+		-EINVAL,
+		0,
+		0,
+		-EBUSY,
+		0,
+		0,
+		0,
+		0,
+	};
+
+	enum ihklib_os_status status_expected[] = {
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+		IHK_STATUS_INACTIVE,
+	};
+
+	int num_os_instances_after_shutdown[] = { 0, 0 };
+
+	int fd;
+	unsigned long os_set[1] = { 1 };
+
+	/* Activate and check */
+	for (i = 0; i < 4; i++) {
+		START("test-case: : %s\n", messages[i]);
+
+		ret = ihk_create_os(0);
+		INTERR(ret, "ihk_create_os returned %d\n", ret);
+
+		ret = cpus_os_assign();
+		INTERR(ret, "cpus_os_assign returned %d\n", ret);
+
+		ret = mems_os_assign();
+		INTERR(ret, "mems_os_assign returned %d\n", ret);
+
+		ret = os_load();
+		INTERR(ret, "os_load returned %d\n", ret);
+
+		ret = os_kargs();
+		INTERR(ret, "os_kargs returned %d\n", ret);
+
+		switch (target_status[i]) {
+		case IHK_STATUS_INACTIVE:
+			break;
+		case IHK_STATUS_BOOTING:
+			/* Manually boot because ihk_os_boot wait for RUNNING */
+			fd = ihklib_os_open(0);
+			INTERR(fd < 0, "ihklib_os_open returned %d\n",
+			       fd);
+
+			ret = ioctl(fd, IHK_OS_BOOT, 0);
+			INTERR(ret == -1, "ihk_os_boot: errno: %d\n", errno);
+
+			close(fd);
+			break;
+		case IHK_STATUS_RUNNING:
+		case IHK_STATUS_SHUTDOWN:
+		case IHK_STATUS_PANIC:
+		case IHK_STATUS_HUNGUP:
+		case IHK_STATUS_FREEZING:
+		case IHK_STATUS_FROZEN:
+			ret = ihk_os_boot(0);
+			INTERR(ret, "ihk_os_boot returned %d\n", ret);
+			break;
+		default:
+			break;
+		}
+
+		switch (target_status[i]) {
+		case IHK_STATUS_SHUTDOWN:
+			pid = fork();
+			if (!pid) {
+				ret = ihk_os_shutdown(0);
+				if (ret) {
+					printf("child: ihk_os_shutdown "
+					       "returned %d\n", ret);
+				}
+				exit(ret);
+			}
+			break;
+		case IHK_STATUS_PANIC:
+			ret = user_fork_exec("panic", &pid);
+			INTERR(ret < 0, "user_fork_exec returned %d\n");
+			break;
+		case IHK_STATUS_HUNGUP:
+			ret = user_fork_exec("hungup", &pid);
+			INTERR(ret < 0, "user_fork_exec returned %d\n");
+
+			/* wait until McKernel start ihk_mc_delay_us() */
+			usleep(0.25 * 1000000);
+
+			fd = ihklib_os_open(0);
+			INTERR(fd < 0, "ihklib_os_open returned %d\n",
+			       fd);
+
+			ioctl(fd, IHK_OS_DETECT_HUNGUP);
+			usleep(0.25 * 1000000);
+			ioctl(fd, IHK_OS_DETECT_HUNGUP);
+
+			close(fd);
+			break;
+		case IHK_STATUS_FREEZING:
+		case IHK_STATUS_FROZEN:
+			ihk_os_freeze(os_set, sizeof(unsigned long) * 8);
+			break;
+		default:
+			break;
+		}
+
+		/* wait until os status changes to the target status */
+		ret = wait_for_status(target_status[i]);
+		INTERR(ret, "os status didn't change to %d\n",
+		       target_status[i]);
+
+ status_changed:
+		INFO("trying to shutdown os\n");
+
+		ret = ihk_os_shutdown(0);
+		OKNG(ret == ret_expected[i],
+		     "return value: %d, expected: %d\n",
+		     ret, ret_expected[i]);
+
+		/* wait until os status stablizes */
+		wait_for_status(status_expected[i]);
+		ret = ihk_os_get_status(0);
+		OKNG(ret == status_expected[i],
+		     "status: %d, expected: %d\n",
+		     ret, status_expected[i]);
+
+		/* Clean up */
+
+		switch (target_status[i]) {
+		case IHK_STATUS_HUNGUP:
+		case IHK_STATUS_PANIC:
+			ret = user_wait(&pid);
+			INTERR(ret, "user_wait returned %d\n", ret);
+			break;
+		default:
+			break;
+		}
+
+		ret = cpus_os_release();
+		INTERR(ret, "cpus_os_assign returned %d\n", ret);
+
+		ret = mems_os_release();
+		INTERR(ret, "mems_os_assign returned %d\n", ret);
+
+		ret = ihk_destroy_os(0, 0);
+		INTERR(ret, "ihk_destroy_os returned %d\n", ret);
+	}
+
+	ret = 0;
+ out:
+	if (pid > 0) {
+		user_wait(&pid);
+	}
+
+	if (ihk_get_num_os_instances(0)) {
+		unsigned long os_set[1] = { 1 };
+
+		switch (ihk_os_get_status(0)) {
+		case IHK_STATUS_FREEZING:
+			wait_for_status(IHK_STATUS_FROZEN);
+			/* fall through */
+		case IHK_STATUS_FROZEN:
+			ihk_os_thaw(os_set, sizeof(unsigned long) * 8);
+			break;
+		default:
+			break;
+		}
+
+		ihk_os_shutdown(0);
+		wait_for_status(IHK_STATUS_INACTIVE);
+		ihk_destroy_os(0, 0);
+	}
+	cpus_release();
+	mems_release();
+	rmmod(1);
+
+	return ret;
+}
+
