0b7efd3fa17af73cc817d1bc45eafa85e5527818
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-18 04:05:34 +0000

    Add ihk_os_get_ikc_map01-06
    Change-Id: Idb22a0cc860e37430855fccff1f543e2fee234ef

---
+++ test/ihklib/blackbox/src/ihk_os_get_ikc_map06.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <ihklib.h>
+#include "util.h"
+#include "okng.h"
+#include "cpu.h"
+#include "params.h"
+#include "mod.h"
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int i;
+
+	params_getopt(argc, argv);
+
+	/* Parse additional options */
+	int opt;
+
+	while ((opt = getopt(argc, argv, "ir")) != -1) {
+		switch (opt) {
+		case 'i':
+			/* Precondition */
+			ret = insmod();
+			INTERR(ret, "insmod returned %d\n", ret);
+			exit(0);
+			break;
+		case 'r':
+			/* Clean up */
+			ret = rmmod(1);
+			INTERR(ret, "rmmod returned %d\n", ret);
+			exit(0);
+			break;
+		default: /* '?' */
+			printf("unknown option %c\n", optopt);
+			exit(1);
+		}
+	}
+
+	const char *values[] = {
+		 "non-root",
+		};
+
+	struct cpus cpus_input[1] = { 0 };
+
+	/* Both Linux and McKernel cpus */
+	for (i = 0; i < 1; i++) {
+		ret = cpus_reserved(&cpus_input[i]);
+		INTERR(ret, "cpus_reserved returned %d\n", ret);
+	}
+
+	/* Spare two cpus for Linux */
+	for (i = 0; i < 1; i++) {
+		ret = cpus_shift(&cpus_input[i], 2);
+		INTERR(ret, "cpus_shift returned %d\n", ret);
+	}
+
+	int ret_expected[] = { -EACCES };
+
+	struct cpus *cpus_expected[] = {
+		 NULL, /* don't care */
+		};
+
+	/* Activate and check */
+	for (i = 0; i < 1; i++) {
+		START("test-case: user privilege: %s\n", values[i]);
+
+		ret = ihk_os_assign_cpu(0, cpus_input[i].cpus, cpus_input[i].ncpus);
+		OKNG(ret == ret_expected[i],
+		     "return value: %d, expected: %d\n",
+		     ret, ret_expected[i]);
+
+		if (cpus_expected[i]) {
+			ret = cpus_check_assigned(cpus_expected[i]);
+			OKNG(ret == 0, "assigned as expected\n");
+
+			/* Clean up */
+			ret = ihk_os_release_cpu(0, cpus_input[i].cpus,
+					      cpus_input[i].ncpus);
+			INTERR(ret, "ihk_os_release_cpu returned %d\n", ret);
+		}
+	}
+
+	ret = 0;
+ out:
+	return ret;
+}
