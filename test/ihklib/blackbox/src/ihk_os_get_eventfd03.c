112f8d822e644243acb18f8f0f0157ffdeb4195c
Jie Yin <jie.yin@riken.jp>
2020-02-18 07:20:12 +0000

    Add ihk_os_get_eventfd01, 02, 03
    Change-Id: I550c46574cad67552f08963bcb2a06591c229432

---
+++ test/ihklib/blackbox/src/ihk_os_get_eventfd03.c
@@ -0,0 +1,87 @@
+#include <limits.h>
+#include <errno.h>
+#include <ihklib.h>
+#include "util.h"
+#include "okng.h"
+#include "cpu.h"
+#include "mem.h"
+#include "os.h"
+#include "params.h"
+#include "mod.h"
+
+const char param[] = "os status";
+const char *values[] = {
+	"before boot",
+	"after boot",
+};
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int i;
+
+	params_getopt(argc, argv);
+
+	/* Precondition */
+	ret = insmod();
+	INTERR(ret, "insmod returned %d\n", ret);
+
+	ret = cpus_reserve();
+	INTERR(ret, "cpus_reserve returned %d\n", ret);
+
+	ret = mems_reserve();
+	INTERR(ret, "mems_reserve returned %d\n", ret);
+
+	ret = ihk_create_os(0);
+	INTERR(ret, "ihk_create_os returned %d\n", ret);
+
+	ret = cpus_os_assign();
+	INTERR(ret, "cpus_os_assign returned %d\n", ret);
+
+	ret = mems_os_assign();
+	INTERR(ret, "mems_os_assign returned %d\n", ret);
+
+	int ret_expected[] = {
+		4,
+		5,
+	};
+
+	/* Activate and check */
+	for (i = 0; i < 2; i++) {
+		START("test-case: %s: %s\n", param, values[i]);
+		/* Precondition */
+		if (i == 1) {
+			ret = os_load();
+			INTERR(ret, "os_load returned %d\n", ret);
+
+			ret = os_kargs();
+			INTERR(ret, "os_kargs returned %d\n", ret);
+
+			ret = ihk_os_boot(0);
+			INTERR(ret, "ihk_os_boot returned %d\n", ret);
+		}
+
+		ret = ihk_os_get_eventfd(0, IHK_OS_EVENTFD_TYPE_OOM);
+		OKNG(ret == ret_expected[i],
+				"return value: %d, expected: %d\n",
+				ret, ret_expected[i]);
+
+		/* Clean up */
+		if (i == 1) {
+			ret = ihk_os_shutdown(0);
+			INTERR(ret, "ihk_os_boot returned %d\n", ret);
+		}
+
+	}
+
+	ret = 0;
+ out:
+	cpus_os_release();
+	mems_os_release();
+	ihk_destroy_os(0, 0);
+	cpus_release();
+	mems_release();
+	rmmod(0);
+
+	return ret;
+}
