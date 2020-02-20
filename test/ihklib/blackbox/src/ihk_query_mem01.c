96d0dc27bc1e35c092518afc3cdf310ac90a1c8e
Jie Yin <jie.yin@riken.jp>
2020-02-20 06:17:26 +0000

    Add ihk_query_mem01
    Change-Id: Ie3950231f32000a02bc2b780b57bca19602e434f

--- test/ihklib/blackbox/src/ihk_query_mem01.c
+++ test/ihklib/blackbox/src/ihk_query_mem01.c
@@ -2,7 +2,7 @@
 #include <ihklib.h>
 #include "util.h"
 #include "okng.h"
-#include "cpu.h"
+#include "mem.h"
 #include "params.h"
 #include "linux.h"
 
@@ -19,72 +19,80 @@ int main(int argc, char **argv)
 
 	params_getopt(argc, argv);
 
-	/* All of McKernel CPUs */
-	struct cpus cpus_input[2] = { 0 };
+	struct mems mems_input[2] = { 0 };
 
-	for (i = 1; i < 2; i++) {
-		ret = cpus_ls(&cpus_input[i]);
-		INTERR(ret, "cpus_ls returned %d\n", ret);
+	/* Reference for sum of the sizes */
+	struct mems mems_expected_size[2] = { 0 };
 
-		ret = cpus_shift(&cpus_input[i], 2);
-		INTERR(ret, "cpus_shift returned %d\n", ret);
+	for (i = 0; i < 2; i++) {
+		int excess;
+
+		ret = mems_ls(&mems_expected_size[i], "MemFree", 0.9);
+		INTERR(ret, "mems_ls returned %d\n", ret);
+
+		excess = mems_expected_size[i].num_mem_chunks - 4;
+		if (excess > 0) {
+			ret = mems_shift(&mems_expected_size[i], excess);
+			INTERR(ret, "mems_ls returned %d\n", ret);
+		}
 	}
 
-	int ret_expected_reserve_cpu[] = { -ENOENT, 0 };
-	int ret_expected_get_num_reserved_cpus[] = {
+	/* Reference for # of memory chunks */
+	struct mems mems_expected_num_mem_chunks[2] = { 0 };
+
+	struct mems *mems_expected[] = { NULL, &mems_expected_size[1] };
+
+	int ret_expected_get_num[] = {
 		-ENOENT,
-		cpus_input[1].ncpus
+		0 /* filled in the loop */
 	};
 	int ret_expected[] = { -ENOENT, 0 };
-	struct cpus *cpus_expected[] = { NULL, &cpus_input[1] };
 
 	/* Activate and check */
 	for (i = 0; i < 2; i++) {
-		struct cpus cpus;
 
-		START("test-case: %s: %s\n", param, messages[i]);
-
-		ret = ihk_reserve_cpu(0, cpus_input[i].cpus,
-				      cpus_input[i].ncpus);
-		INTERR(ret != ret_expected_reserve_cpu[i],
-		       "ihk_reserve_cpu returned %d\n", ret);
-
-		ret = ihk_get_num_reserved_cpus(0);
-		INTERR(ret != ret_expected_get_num_reserved_cpus[i],
-		       "ihk_get_num_reserved_cpus returned %d\n", ret);
-
-		if (!cpus_expected[i]) {
-			ret = cpus_init(&cpus, 1);
-			INTERR(ret, "cpus_init returned %d\n", ret);
-
-			ret = ihk_query_cpu(0, cpus.cpus, cpus.ncpus);
-			OKNG(ret == ret_expected[i],
-			     "return value: %d, expected: %d\n",
-			     ret, ret_expected[i]);
-		} else {
-			cpus.ncpus = ret;
-
-			ret = cpus_init(&cpus, cpus.ncpus);
-			INTERR(ret, "cpus_init returned %d\n", ret);
-
-			ret = ihk_query_cpu(0, cpus.cpus, cpus.ncpus);
-			OKNG(ret == ret_expected[i],
-			     "return value: %d, expected: %d\n",
-			     ret, ret_expected[i]);
-
-			ret = cpus_compare(&cpus, cpus_expected[i]);
-			OKNG(ret == 0, "query result matches input\n");
-
-			/* Clean up */
-			ret = ihk_release_cpu(0, cpus_input[i].cpus,
-					      cpus_input[i].ncpus);
-			INTERR(ret, "ihk_release_cpu returned %d\n", ret);
-		}
+		START("test-case: %s: %s\n", param, values[i]);
 
 		/* Precondition */
-		if (i == 0) {
+		if (i == 1) {
 			ret = linux_insmod();
-			INTERR(ret == 0, "linux_insmod returned %d\n", ret);
+			INTERR(ret, "linux_insmod returned %d\n", ret);
+
+			ret = ihk_reserve_mem(0, mems_expected_size[i].mem_chunks,
+					      mems_expected_size[i].num_mem_chunks);
+			INTERR(ret, "ihk_reserve_mem returned %d\n", ret);
+
+			ret = mems_reserved(&mems_input[i]);
+			INTERR(ret, "mems_reserved returned %d\n", ret);
+
+			ret = mems_reserved(&mems_expected_num_mem_chunks[i]);
+			INTERR(ret, "mems_reserved returned %d\n", ret);
+			ret_expected_get_num[i] =
+				mems_expected_num_mem_chunks[1].num_mem_chunks;
+		}
+
+		ret = ihk_get_num_reserved_mem_chunks(0);
+		INTERR(ret != ret_expected_get_num[i],
+		       "ihk_get_num_reserved_mems returned %d\n", ret);
+		mems_input[i].num_mem_chunks = ret;
+
+		ret = ihk_query_mem(0, mems_input[i].mem_chunks,
+				mems_input[i].num_mem_chunks);
+		OKNG(ret == ret_expected[i],
+			"return value: %d, expected: %d\n",
+			ret, ret_expected[i]);
+
+		if (mems_expected[i]) {
+			/* Note that only per-NUMA-node-sum is compared */
+			ret = mems_compare(&mems_input[i], mems_expected[i],
+					   NULL);
+			OKNG(ret == 0, "query result matches reserved\n");
+		}
+
+		/* Clean up */
+		if (i == 1) {
+			ret = mems_release();
+			INTERR(ret, "mems_release returned %d\n", ret);
 		}
 	}
 
