425f64efb5b9f6d3f5d67472573d1849706e845b
Jie Yin <jie.yin@riken.jp>
2020-02-21 06:23:00 +0000

    Add ihk_query_mem03-07
    Change-Id: Ib34ba91d339ae9bac9df729f1eedd9926f8d6d5e

--- test/ihklib/blackbox/src/ihk_query_mem06.c
+++ test/ihklib/blackbox/src/ihk_query_mem06.c
@@ -4,10 +4,11 @@
 #include <ihklib.h>
 #include "util.h"
 #include "okng.h"
-#include "cpu.h"
+#include "mem.h"
 #include "params.h"
 #include "linux.h"
 
+const char param[] = "user privilege";
 const char *values[] = {
 	"non-root",
 };
@@ -28,10 +29,16 @@ int main(int argc, char **argv)
 			/* Precondition */
 			ret = linux_insmod();
 			INTERR(ret, "linux_insmod returned %d\n", ret);
+
+			ret = mems_reserve();
+			INTERR(ret, "mems_reserve returned %d\n", ret);
 			exit(0);
 			break;
 		case 'r':
 			/* Clean up */
+			ret = mems_release();
+			INTERR(ret, "mems_release returned %d\n", ret);
+
 			ret = linux_rmmod(1);
 			INTERR(ret, "rmmod returned %d\n", ret);
 			exit(0);
@@ -42,55 +49,30 @@ int main(int argc, char **argv)
 		}
 	}
 
-	struct cpus cpus_input_reserve_cpu[1] = { 0 };
+	struct mems mems_input[1] = { 0 };
 
-	/* Both Linux and McKernel cpus */
-	for (i = 0; i < 1; i++) {
-		ret = cpus_ls(&cpus_input_reserve_cpu[i]);
-		INTERR(ret, "cpus_ls returned %d\n", ret);
-	}
-
-	/* Spare two cpus for Linux */
-	for (i = 0; i < 1; i++) {
-		ret = cpus_shift(&cpus_input_reserve_cpu[i], 2);
-		INTERR(ret, "cpus_shift returned %d\n", ret);
-	}
-
-	struct cpus cpus_input[] = { 0 };
-	ret = cpus_init(&cpus_input[0], cpus_input_reserve_cpu[0].ncpus);
-	INTERR(ret, "cpus_init returned %d\n", ret);
-
-	int ret_expected_reserve_cpu[] = { -EACCES };
 	int ret_expected[] = { -EACCES };
-
-	struct cpus *cpus_expected[] = {
-		 NULL, /* don't care */
-		};
+	int ret_expected_get_num_reserved[] = { -EACCES };
 
 	/* Activate and check */
 	for (i = 0; i < 1; i++) {
-		START("test-case: user privilege: %s\n", values[i]);
+		int num_mem_chunks;
 
-		ret = ihk_reserve_cpu(0, cpus_input[i].cpus,
-				      cpus_input[i].ncpus);
-		INTERR(ret != ret_expected_reserve_cpu[i],
-		     "ihk_reserve_cpu returned %d\n", ret);
+		START("test-case: %s: %s\n", param, values[i]);
 
-		ret = ihk_query_cpu(0, cpus_input[i].cpus,
-				    cpus_input[i].ncpus);
+		ret = ihk_get_num_reserved_mem_chunks(0);
+		INTERR(ret != ret_expected_get_num_reserved[i],
+			"ihk_get_num_reserved_mems returned %d\n", ret);
+		num_mem_chunks = (ret < 0) ? 0 : ret;
+
+		ret = mems_init(&mems_input[0], num_mem_chunks);
+		INTERR(ret, "mems_init returned %d\n", ret);
+
+		ret = ihk_query_mem(0, mems_input[i].mem_chunks,
+				    mems_input[i].num_mem_chunks);
 		OKNG(ret == ret_expected[i],
 		     "return value: %d, expected: %d\n",
 		     ret, ret_expected[i]);
-
-		if (cpus_expected[i]) {
-			ret = cpus_check_reserved(cpus_expected[i]);
-			OKNG(ret == 0, "reserved as expected\n");
-
-			/* Clean up */
-			ret = ihk_release_cpu(0, cpus_input_reserve_cpu[i].cpus,
-					      cpus_input_reserve_cpu[i].ncpus);
-			INTERR(ret, "ihk_release_cpu returned %d\n", ret);
-		}
 	}
 
 	ret = 0;
