ec94b25231c673df427e7d2338fc5c01af63b44b
Jie Yin <jie.yin@riken.jp>
2020-02-26 04:50:43 +0000

    Add ihk_os_assign_mem05-07
    Change-Id: I8f3f109a0ad541ab7635116de677847beed733b1

--- test/ihklib/blackbox/src/ihk_os_assign_mem05.c
+++ test/ihklib/blackbox/src/ihk_os_assign_mem05.c
@@ -7,6 +7,7 @@
 #include "params.h"
 #include "linux.h"
 
+const char param[] = "user privilege";
 const char *values[] = {
 	"root",
 };
@@ -18,33 +19,37 @@ int main(int argc, char **argv)
 
 	params_getopt(argc, argv);
 
-	struct mems mems_input[1] = { 0 };
-
-	/* Both Linux and McKernel cpus */
-	for (i = 0; i < 1; i++) {
-		ret = mems_ls(&mems_after_assign[i]);
-		INTERR(ret, "mems_ls returned %d\n", ret);
-	}
-
-	/* Spare two cpus for Linux */
-	for (i = 0; i < 1; i++) {
-		ret = mems_shift(&mems_after_assign[i], 2);
-		INTERR(ret, "mems_shift returned %d\n", ret);
-	}
-
-	int ret_expected[] = { 0 };
-
-	struct mems *mems_expected[] = {
-		&mems_after_assign[0],
-	};
-
 	/* Precondition */
 	ret = linux_insmod();
 	INTERR(ret, "linux_insmod returned %d\n", ret);
 
+	ret = mems_reserve();
+	INTERR(ret, "mems_reserve returned %d\n", ret);
+
+	ret = ihk_create_os(0);
+	INTERR(ret, "ihk_create_os returned %d\n", ret);
+
+	struct mems mems_input[1] = { 0 };
+	struct mems mems_after_assign[1] = { 0 };
+
+	/* Both Linux and McKernel cpus */
+	for (i = 0; i < 1; i++) {
+		ret = mems_reserved(&mems_input[i]);
+		INTERR(ret, "mems_reserved returned %d\n", ret);
+
+		ret = mems_reserved(&mems_after_assign[i]);
+		INTERR(ret, "mems_reserved returned %d\n", ret);
+	}
+
+	int ret_expected[1] = { 0 };
+
+	struct mems *mems_expected[1] = {
+		&mems_after_assign[0],
+	};
+
 	/* Activate and check */
 	for (i = 0; i < 1; i++) {
-		START("test-case: user privilege: %s\n", values[i]);
+		START("test-case: %s: %s\n", param, values[i]);
 
 		ret = ihk_os_assign_mem(0, mems_input[i].mem_chunks,
 				      mems_input[i].num_mem_chunks);
@@ -57,14 +62,14 @@ int main(int argc, char **argv)
 			OKNG(ret == 0, "assigned as expected\n");
 
 			/* Clean up */
-			ret = mems_os_release(0, mems_input[i].mem_chunks,
-					      mems_input[i].num_mem_chunks);
+			ret = mems_os_release();
 			INTERR(ret, "mems_os_release returned %d\n", ret);
 		}
 	}
 
 	ret = 0;
  out:
+	mems_release();
 	linux_rmmod(0);
 	return ret;
 }
--- test/ihklib/blackbox/src/ihk_os_assign_mem06.c
+++ test/ihklib/blackbox/src/ihk_os_assign_mem06.c
@@ -28,10 +28,23 @@ int main(int argc, char **argv)
 			/* Precondition */
 			ret = linux_insmod();
 			INTERR(ret, "linux_insmod returned %d\n", ret);
+
+			ret = mems_reserve();
+			INTERR(ret, "mems_reserve returned %d\n", ret);
+
+			ret = ihk_create_os(0);
+			INTERR(ret, "ihk_create_os returned %d\n", ret);
+
+			ret = linux_chmod(params.uid, params.gid);
+			INTERR(ret, "linux_chmod returned %d\n", ret);
+
 			exit(0);
 			break;
 		case 'r':
 			/* Clean up */
+			ret = mems_release();
+			INTERR(ret, "mems_release returned %d\n", ret);
+
 			ret = linux_rmmod(1);
 			INTERR(ret, "rmmod returned %d\n", ret);
 			exit(0);
@@ -43,44 +56,20 @@ int main(int argc, char **argv)
 	}
 
 	struct mems mems_input[1] = { 0 };
-
-	/* Both Linux and McKernel cpus */
-	for (i = 0; i < 1; i++) {
-		ret = mems_ls(&mems_after_assign[i]);
-		INTERR(ret, "mems_ls returned %d\n", ret);
-	}
-
-	/* Spare two cpus for Linux */
-	for (i = 0; i < 1; i++) {
-		ret = mems_shift(&mems_after_assign[i], 2);
-		INTERR(ret, "mems_shift returned %d\n", ret);
-	}
-
-	int ret_expected[] = { -EACCES };
-
-	struct mems *mems_expected[] = {
-		 NULL, /* don't care */
-	};
+	int ret_expected[] = { -EPERM };
 
 	/* Activate and check */
 	for (i = 0; i < 1; i++) {
 		START("test-case: user privilege: %s\n", values[i]);
+
+		ret = mems_push(&mems_input[i], -1, 0);
+		INTERR(ret, "mems_push returned %d\n", ret);
 
 		ret = ihk_os_assign_mem(0, mems_input[i].mem_chunks,
 				      mems_input[i].num_mem_chunks);
 		OKNG(ret == ret_expected[i],
 		     "return value: %d, expected: %d\n",
 		     ret, ret_expected[i]);
-
-		if (mems_expected[i]) {
-			ret = mems_check_assigned(mems_expected[i]);
-			OKNG(ret == 0, "assigned as expected\n");
-
-			/* Clean up */
-			ret = mems_os_release(0, mems_input[i].mem_chunks,
-					      mems_input[i].num_mem_chunks);
-			INTERR(ret, "mems_os_release returned %d\n", ret);
-		}
 	}
 
 	ret = 0;
