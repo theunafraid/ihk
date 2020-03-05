fcc8f0ce0530f0c64b46af496a381c916d25aedc
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-03-05 06:42:44 +0000

    fix linux.c
    Change-Id: Ibc1a657db423527a87cc473efe0d0acaea176437

--- test/ihklib/blackbox/include/linux.h
+++ test/ihklib/blackbox/include/linux.h
@@ -2,9 +2,7 @@
 #define __LINUX_H_INCLUDED__
 
 int linux_insmod(void);
-int linux_testchmod(int);
-int linux_chmod(uid_t uid, gid_t git);
-int linux_wait_for_permission(int index);
+int linux_chmod(int dev_index);
 int linux_rmmod(int verbose);
 int linux_kill_mcexec(void);
 
