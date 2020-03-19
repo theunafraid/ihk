7a5057381dfcd34f4aa47094c1b17ff3701ed2af
Jie Yin <jie.yin@riken.jp>
2020-03-19 08:45:32 +0000

    Fix ihk_os_set_ikc_map07
    Change-Id: Idaceebc75f9dbae5c63ee53c6220d7a2af01249d

--- test/ihklib/blackbox/src/ihk_os_set_ikc_map07.c
+++ test/ihklib/blackbox/src/ihk_os_set_ikc_map07.c
@@ -47,9 +47,6 @@ int main(int argc, char **argv)
 
 	/* Activate and check */
 	for (i = 0; i < 1; i++) {
-		int errno_save;
-		int ncpu;
-		char cmd[4096];
 
 		START("test-case: %s: %s\n", param, values[i]);
 
