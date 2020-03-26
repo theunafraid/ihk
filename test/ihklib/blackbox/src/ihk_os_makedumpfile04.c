cf2594565d72fbe3fe8cac6fc261295c6a9ee3bb
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-03-26 18:30:36 -0400

    Add ihk_os_makedumpfile04

--- test/ihklib/blackbox/src/ihk_os_makedumpfile04.c
+++ test/ihklib/blackbox/src/ihk_os_makedumpfile04.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <errno.h>
 #include <ihklib.h>
 #include "util.h"
@@ -14,7 +15,7 @@ const char param[] = "Path of dumpfiles";
 const char *values[] = {
 	"NULL",
 	"valid path",
-	"path with invalid directory",
+	"path with non-existent directory",
 	"inaccessible path"
 };
 
@@ -24,13 +25,20 @@ int main(int argc, char **argv)
 	int i;
 	char *fn;
 	int opt;
+	int index;
 
 	params_getopt(argc, argv);
 
-	while ((opt = getopt(argc, argv, "f:")) != -1) {
+	while ((opt = getopt(argc, argv, "i:f:")) != -1) {
 		switch (opt) {
+		case 'i':
+			index = atoi(optarg);
+			break;
 		case 'f':
 			fn = optarg;
+			if (!strncmp(fn, "NULL", 5)) {
+				fn = NULL;
+			}
 			break;
 		default: /* '?' */
 			printf("unknown option %c\n", optopt);
@@ -38,23 +46,18 @@ int main(int argc, char **argv)
 		}
 	}
 
-	char *dumpfile_input[4] = {
-		NULL,
-		fn,
-		"/path/to/nonexistence",
-		NULL
-	};
-
 	int ret_expected[4] = {
-		-EINVAL,
+		-EFAULT,
 		0,
 		-ENOENT,
-		-EACCES,
+		-EPERM,
 	};
 
 	int ret_access_expected[4] = {
 		-1,
-		0
+		0,
+		-1,
+		-1,
 	};
 
 	/* Precondition */
@@ -64,75 +67,76 @@ int main(int argc, char **argv)
 	ret = cpus_reserve();
 	INTERR(ret, "cpus_reserve returned %d\n", ret);
 
-	ret = mems_reserve();
-	INTERR(ret, "mems_reserve returned %d\n", ret);
+	struct mems mems = { 0 };
+	int excess;
+
+	ret = mems_ls(&mems, "MemFree", 0.9);
+	INTERR(ret, "mems_ls returned %d\n", ret);
+
+	excess = mems.num_mem_chunks - 4;
+	if (excess > 0) {
+		ret = mems_shift(&mems, excess);
+		INTERR(ret, "mems_shift returned %d\n", ret);
+	}
+
+	mems_fill(&mems, (1UL << 29) / mems.num_mem_chunks);
+
+	ret = ihk_reserve_mem(0, mems.mem_chunks,
+			      mems.num_mem_chunks);
+	INTERR(ret, "ihk_reserve_mem returned %d\n", ret);
 
 	/* Activate and check */
-	for (i = 0; i < 4; i++) {
-		START("test-case: %s: %s\n", param, values[i]);
+	START("test-case: %s: %s\n", param, values[index]);
 
-		ret = ihk_create_os(0);
-		INTERR(ret, "ihk_create_os returned %d\n", ret);
+	ret = ihk_create_os(0);
+	INTERR(ret, "ihk_create_os returned %d\n", ret);
 
-		ret = cpus_os_assign();
-		INTERR(ret, "cpus_os_assign returned %d\n", ret);
+	ret = cpus_os_assign();
+	INTERR(ret, "cpus_os_assign returned %d\n", ret);
 
-		ret = mems_os_assign();
-		INTERR(ret, "mems_os_assign returned %d\n", ret);
+	ret = mems_os_assign();
+	INTERR(ret, "mems_os_assign returned %d\n", ret);
 
-		ret = os_load();
-		INTERR(ret, "os_load returned %d\n", ret);
+	ret = os_load();
+	INTERR(ret, "os_load returned %d\n", ret);
 
-		ret = os_kargs();
-		INTERR(ret, "os_kargs returned %d\n", ret);
+	ret = os_kargs();
+	INTERR(ret, "os_kargs returned %d\n", ret);
 
-		/* Precondition */
-		if (i == 1) {
-			ret = ihk_os_boot(0);
-			INTERR(ret, "ihk_os_boot returned %d\n", ret);
+	ret = ihk_os_boot(0);
+	INTERR(ret, "ihk_os_boot returned %d\n", ret);
 
-			ret = os_wait_for_status(IHK_STATUS_RUNNING);
-			INTERR(ret, "os status didn't change to %d\n",
-			       IHK_STATUS_RUNNING);
-		}
+	ret = ihk_os_makedumpfile(0, fn, 24, 0);
+	OKNG(ret == ret_expected[index],
+	     "return value: %d, expected: %d\n",
+	     ret, ret_expected[index]);
 
-		ret = ihk_os_makedumpfile(0, dumpfile_input, 24, 0);
-		OKNG(ret == ret_expected[i],
-		     "return value: %d, expected: %d\n",
-		     ret, ret_expected[i]);
+	ret = access(fn, F_OK);
+	OKNG(ret == ret_access_expected[index],
+	     "dumpfile %screated as expected\n",
+	     ret_access_expected[index] == 0 ? "" : "not ");
 
-		ret = access(dumpfile_input, F_OK);
-		OKNG(ret == ret_access_expected[i],
-			"dumpfile created as expected\n");
+	/* Clean up */
+	if ((ihk_os_get_status(0)) == IHK_STATUS_RUNNING) {
+		ret = ihk_os_shutdown(0);
+		INTERR(ret, "ihk_os_shutdown returned %d\n", ret);
 
-		/* Clean up */
-		if ((ihk_os_get_status(0)) == IHK_STATUS_RUNNING) {
-			ret = unlink(dumpfile_input);
-			INTERR(ret, "unlink returned %d\n", ret);
-
-			ret = ihk_os_shutdown(0);
-			INTERR(ret, "ihk_os_shutdown returned %d\n", ret);
-
-			ret = os_wait_for_status(IHK_STATUS_INACTIVE);
-			INTERR(ret, "os status didn't change to %d\n",
-			       IHK_STATUS_INACTIVE);
-		}
-
-		ret = cpus_os_release();
-		INTERR(ret, "cpus_os_release returned %d\n", ret);
-
-		ret = mems_os_release();
-		INTERR(ret, "mems_os_release returned %d\n", ret);
-
-		ret = ihk_destroy_os(0, 0);
-		INTERR(ret, "ihk_destroy_os returned %d\n", ret);
+		ret = os_wait_for_status(IHK_STATUS_INACTIVE);
+		INTERR(ret, "os status didn't change to %d\n",
+		       IHK_STATUS_INACTIVE);
 	}
+
+	ret = cpus_os_release();
+	INTERR(ret, "cpus_os_release returned %d\n", ret);
+
+	ret = mems_os_release();
+	INTERR(ret, "mems_os_release returned %d\n", ret);
+
+	ret = ihk_destroy_os(0, 0);
+	INTERR(ret, "ihk_destroy_os returned %d\n", ret);
 
 	ret = 0;
  out:
-	if (!(access(dumpfile_input, F_OK))) {
-		unlink(dumpfile_input);
-	}
 	if (ihk_get_num_os_instances(0)) {
 		ihk_os_shutdown(0);
 		os_wait_for_status(IHK_STATUS_INACTIVE);
