d8c2aba5e34fd07fd0e07962f5fc86350636d1f6
Jie Yin <jie.yin@riken.jp>
2020-04-02 07:47:37 +0000

    (wip) ihk_os_getrusage09.c
    Change-Id: Ifdf585a98c294e2aa1e6cb343c18ee728d82907a

---
+++ test/ihklib/blackbox/src/ihk_os_getrusage09.c
@@ -0,0 +1,214 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <ihklib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "util.h"
+#include "okng.h"
+#include "cpu.h"
+#include "mem.h"
+#include "os.h"
+#include "params.h"
+#include "linux.h"
+#include "user.h"
+
+const char param[] = "memory_numa_stat";
+const char *values[] = {
+	"256MB at node 0 and 512MB at node 1"
+};
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int i;
+	int fd_in, fd_out;
+	char *fn_in, *fn_out;
+	int opt;
+
+	params_getopt(argc, argv);
+
+	while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+		switch (opt) {
+		case 'i':
+			fn_in = optarg;
+			break;
+		case 'o':
+			fn_out = optarg;
+			break;
+		default: /* '?' */
+			printf("unknown option %c\n", optopt);
+			exit(1);
+		}
+	}
+
+	char *node_size = "0:256MB,1:512MB";
+	//char *node_size = "4:256MB,7:512MB";
+
+	int ret_expected[1] = { 0 };
+
+	struct ihk_os_rusage ru_input_before[1] = { 0 };
+	struct ihk_os_rusage ru_input_after[1] = { 0 };
+
+	struct ihk_os_rusage ru_expected[1] = {
+		{
+			.memory_numa_stat[0] = 256 * 1024 * 1024,
+			.memory_numa_stat[1] = 512 * 1024 * 1024,
+		},
+	};
+
+	/* Precondition */
+	ret = linux_insmod(0);
+	INTERR(ret, "linux_insmod returned %d\n", ret);
+
+	ret = cpus_reserve();
+	INTERR(ret, "cpus_reserve returned %d\n", ret);
+
+	ret = mems_reserve();
+	INTERR(ret, "mems_reserve returned %d\n", ret);
+
+	pid_t pid = -1;
+	int wstatus;
+	int message = 1;
+	char cmd[4096];
+
+	/* Activate and check */
+	for (i = 0; i < 1; i++) {
+
+		START("test-case: %s: %s\n", param, values[i]);
+
+		ret = ihk_create_os(0);
+		INTERR(ret, "ihk_create_os returned %d\n", ret);
+
+		ret = cpus_os_assign();
+		INTERR(ret, "cpus_os_assign returned %d\n", ret);
+
+		ret = mems_os_assign();
+		INTERR(ret, "mems_os_assign returned %d\n", ret);
+
+		ret = os_load();
+		INTERR(ret, "os_load returned %d\n", ret);
+
+		ret = os_kargs();
+		INTERR(ret, "os_kargs returned %d\n", ret);
+
+		ret = ihk_os_boot(0);
+		INTERR(ret, "ihk_os_boot returned %d\n", ret);
+
+		ret = os_wait_for_status(IHK_STATUS_RUNNING);
+		INTERR(ret, "os status didn't change to %d\n",
+		       IHK_STATUS_RUNNING);
+
+		fd_in = open(fn_in, O_RDWR);
+		INTERR(fd_in == -1, "open returned %d\n", errno);
+
+		fd_out = open(fn_out, O_RDWR);
+		INTERR(fd_out == -1, "open returned %d\n", errno);
+
+		sprintf(cmd, "numa_alloc %s %s -n %s",
+			fn_in, fn_out, node_size);
+		ret = user_fork_exec(cmd, &pid);
+		INTERR(ret < 0, "user_fork_exec returned %d\n", ret);
+
+		/* Wait until child is ready */
+		ret = read(fd_out, &message, sizeof(int));
+		INTERR(ret <= 0, "read returned %d, errno: %d\n",
+		       ret, errno);
+
+		ret = ihk_os_getrusage(0, &ru_input_before[i],
+				sizeof(struct ihk_os_rusage));
+		OKNG(ret == ret_expected[i], "return value: %d, expected: %d\n",
+		     ret, ret_expected[i]);
+
+		INFO("node#0: %lu\n",
+			ru_input_before[i].memory_numa_stat[0]);
+		INFO("node#1: %lu\n",
+			ru_input_before[i].memory_numa_stat[1]);
+
+		ret = write(fd_in, &message, sizeof(int));
+		INTERR(ret != sizeof(int),
+		       "write returned %d\n", errno);
+
+		ret = read(fd_out, &message, sizeof(int));
+		INTERR(ret <= 0, "read returned %d, errno: %d\n",
+		       ret, errno);
+
+		ret = ihk_os_getrusage(0, &ru_input_after[i],
+				sizeof(struct ihk_os_rusage));
+		OKNG(ret == ret_expected[i], "return value: %d, expected: %d\n",
+		     ret, ret_expected[i]);
+
+		INFO("node#0: %lu\n", ru_input_after[i].memory_numa_stat[0]);
+		INFO("node#1: %lu\n", ru_input_after[i].memory_numa_stat[1]);
+
+		ret = write(fd_in, &message, sizeof(int));
+		INTERR(ret != sizeof(int), "write returned %d\n", errno);
+
+		ret = waitpid(pid, &wstatus, 0);
+		INTERR(ret < 0, "waitpid returned %d\n", errno);
+		pid = -1;
+
+		close(fd_in);
+		close(fd_out);
+
+		if (ret_expected[i] == 0) {
+			unsigned long node0 =
+			ru_input_after[i].memory_numa_stat[0] -
+			ru_input_before[i].memory_numa_stat[0];
+			unsigned long node1 =
+			ru_input_after[i].memory_numa_stat[1] -
+			ru_input_before[i].memory_numa_stat[1];
+
+			unsigned long node0_expected =
+				ru_expected[i].memory_numa_stat[0];
+			unsigned long node1_expected =
+				ru_expected[i].memory_numa_stat[1];
+
+			OKNG(node0 >= node0_expected &&
+				node0 <= node0_expected * 1.1,
+				"user: %d, expected: %d\n",
+				node0, node0_expected);
+			OKNG(node1 >= node1_expected &&
+				node1 <= node1_expected * 1.1,
+				"user: %d, expected: %d\n",
+				node1, node1_expected);
+		}
+		ret = ihk_os_shutdown(0);
+		INTERR(ret, "ihk_os_shutdown returned %d\n", ret);
+
+		ret = os_wait_for_status(IHK_STATUS_INACTIVE);
+		INTERR(ret, "os status didn't change to %d\n",
+		       IHK_STATUS_INACTIVE);
+
+		ret = cpus_os_release();
+		INTERR(ret, "cpus_os_release returned %d\n", ret);
+
+		ret = mems_os_release();
+		INTERR(ret, "mems_os_release returned %d\n", ret);
+
+		ret = ihk_destroy_os(0, 0);
+		INTERR(ret, "ihk_destroy_os returned %d\n", ret);
+	}
+
+	ret = 0;
+ out:
+	if (pid != -1) {
+		user_wait(&pid);
+		linux_kill_mcexec();
+	}
+	if (ihk_get_num_os_instances(0)) {
+		ihk_os_shutdown(0);
+		os_wait_for_status(IHK_STATUS_INACTIVE);
+		cpus_os_release();
+		mems_os_release();
+		ihk_destroy_os(0, 0);
+	}
+	cpus_release();
+	mems_release();
+	linux_rmmod(1);
+
+	return ret;
+}
